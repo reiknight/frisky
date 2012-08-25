af513726a65e9b2b4c259f6c4c1b18d40acba0a9
David <david.morcillo@gmail.com>
2012-08-25 19:08:49 +0200

    ManagedGameState is not abstract anymore

--- src/infinitedog/frisky/game/ManagedGameState.java
+++ src/infinitedog/frisky/game/ManagedGameState.java
@@ -14,7 +14,7 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public abstract class ManagedGameState extends BasicGameState {
+public class ManagedGameState extends BasicGameState {
     private int stateID = -1;
     protected EntityManager em = EntityManager.getInstance();
     protected PhysicsManager pm = PhysicsManager.getInstance();
@@ -29,5 +29,20 @@ public abstract class ManagedGameState extends BasicGameState {
     @Override
     public int getID() {
         return this.stateID;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
